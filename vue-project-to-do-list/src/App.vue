9939d590d618c2788475619961d7c9ee81110056
Mohammad Pouya Allahyari <pouya3883@gmail.com>
2022-07-02 16:53:19 +0430

    Header added with a little styles & three other components added

--- vue-project-to-do-list/src/App.vue
+++ vue-project-to-do-list/src/App.vue
@@ -1,11 +1,31 @@
 <template>
-  
+  <Header />
 </template>
 
 <script>
+import Header from './components/Header.vue';
 
+export default {
+  data() {
+    return {
+
+    };
+  },
+  components: {
+    Header,
+  },
+};
 </script>
 
 <style>
+@import url('https://fonts.googleapis.com/css2?family=Open+Sans:wght@400;700&display=swap');
+* {
+  margin: 0;
+  padding: 0;
+  box-sizing: border-box;
+}
 
+body {
+  font-family: 'Open Sans', sans-serif;
+}
 </style>
---
+++ vue-project-to-do-list/src/components/Header.vue
@@ -0,0 +1,32 @@
+<template>
+<div class="container">
+  <div>
+    <h1>To-Do-List</h1>
+  </div>
+  <Main />
+  <Footer />
+</div>
+</template>
+
+<script>
+  import Main from './Main.vue'
+  import Footer from './Footer.vue'
+  export default {
+    // name: Header,
+    components: {
+      Main,
+      Footer
+    }
+  }
+</script>
+
+<style scoped>
+  h1 {
+    color: rgb(68, 68, 68);
+  }
+  .container {
+    text-align: center;
+    margin: 100px 200px;
+    border: 1px solid rgb(182, 182, 182);
+  }
+</style>
